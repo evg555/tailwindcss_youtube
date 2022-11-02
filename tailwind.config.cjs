23f0f7f0679b77d3f7b5f133909bd9021589e70d
Evgeniy Krylov <ontropos@mail.ru>
2022-11-02 23:57:34 +0400

    Create page

---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  darkMode: 'class',
+  theme: {
+      screens: {
+          'lg': {'max': '992px'},
+          'md': {'max': '768px'},
+          'sm': {'max': '480px'}
+      },
+      container: {
+          padding: '20px',
+          center: true
+      },
+      extend: {
+          colors: {
+              lightblack: '#4D4244',
+              lightred: '#FF0D38',
+              darkred: '#D70026',
+              lightgrey: '#747474',
+              darkgrey: '#272727'
+          },
+          boxShadow: {
+              customred: '0px 0px 30px rgba(255, 13, 56, 0.21)'
+          }
+      },
+  },
+  plugins: [],
+}
